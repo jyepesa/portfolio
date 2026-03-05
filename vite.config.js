41386d2e0344280c87fd26b4600ac8f67c086003
jyepesa <juank_713@hotmail.com>
2026-03-05 15:41:25 +0100

    finish hero section

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
+const repoName = "portfolio";
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        impressum: "./impressum.html",
+      },
+    },
+  },
 });
